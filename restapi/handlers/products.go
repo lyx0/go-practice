dad61db736aeaaad3e385f73ca996615f8566feb
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-12-04 00:05:04 +0100

    handle get request

--- restapi/handlers/products.go
+++ restapi/handlers/products.go
@@ -1,1 +1,13 @@
 package handlers
+
+import "net/http"
+
+type Products struct{}
+
+func NewProducts() *Products {
+	return &Products{}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Product"))
+}
--- restapi/main.go
+++ restapi/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/lyx0/go-practice/restapi/handlers"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	ph := handlers.NewProducts()
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	http.ListenAndServe(":8080", sm)
+
 }
